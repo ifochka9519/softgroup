85420efcbc0bb11999d27aa8ba9b7e992b2a4e38
Yulya <shplka9519@gmail.com>
2017-01-25 00:45:03 +0200

    Add database dump with some data

--- task1/db/football.sql
+++ task1/db/football.sql
@@ -1,17 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Янв 24 2017 г., 23:41
+-- Версия сервера: 10.1.16-MariaDB
+-- Версия PHP: 5.6.24
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `football`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cities`
+--
 
 CREATE TABLE `cities` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `id_country` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `cities`
+--
+
+INSERT INTO `cities` (`id`, `name`, `id_country`) VALUES
+(1, 'London', 2),
+(2, 'Dnipro', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `clubs`
+--
 
 CREATE TABLE `clubs` (
   `id` int(11) NOT NULL,
@@ -25,10 +58,37 @@ CREATE TABLE `clubs` (
   `revenue` decimal(10,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `clubs`
+--
+
+INSERT INTO `clubs` (`id`, `name`, `id_city`, `id_country`, `id_league`, `year`, `president`, `id_stadium`, `revenue`) VALUES
+(1, 'Dnipro', 2, 1, 1, 1918, 'Ihor Kolomoyskyi', 1, '1000000');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
 CREATE TABLE `countries` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`) VALUES
+(1, 'Ukraine'),
+(2, 'England');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `leagues`
+--
 
 CREATE TABLE `leagues` (
   `id` int(11) NOT NULL,
@@ -38,19 +98,51 @@ CREATE TABLE `leagues` (
   `president` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `leagues`
+--
+
+INSERT INTO `leagues` (`id`, `name`, `id_country`, `top_place`, `president`) VALUES
+(1, 'Premier League', 1, 10, 'Volodymyr Heninson');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `stadiums`
+--
+
 CREATE TABLE `stadiums` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `year` year(4) NOT NULL,
   `capacity` int(11) NOT NULL,
   `id_city` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `stadiums`
+--
+
+INSERT INTO `stadiums` (`id`, `name`, `year`, `capacity`, `id_city`) VALUES
+(1, 'Dnipro-Arena', 2008, 31003, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `trophy`
+--
 
 CREATE TABLE `trophy` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `year` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `victory`
+--
 
 CREATE TABLE `victory` (
   `id` int(11) NOT NULL,
@@ -59,41 +151,89 @@ CREATE TABLE `victory` (
   `year` year(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Индексы сохранённых таблиц
+--
 
+--
+-- Индексы таблицы `cities`
+--
 ALTER TABLE `cities`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `clubs`
+--
 ALTER TABLE `clubs`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `countries`
+--
 ALTER TABLE `countries`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `leagues`
+--
 ALTER TABLE `leagues`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `stadiums`
+--
 ALTER TABLE `stadiums`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `trophy`
+--
 ALTER TABLE `trophy`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Индексы таблицы `victory`
+--
 ALTER TABLE `victory`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
 
+--
+-- AUTO_INCREMENT для таблицы `cities`
+--
 ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `clubs`
+--
 ALTER TABLE `clubs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `countries`
+--
 ALTER TABLE `countries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `leagues`
+--
 ALTER TABLE `leagues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `stadiums`
+--
 ALTER TABLE `stadiums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `trophy`
+--
 ALTER TABLE `trophy`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `victory`
+--
 ALTER TABLE `victory`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
